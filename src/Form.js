ce8ea3d973802569a9d238cd58e56791c9d316ea
Gabby Venegas <gabbyvenegas@Gabbys-MBP.attlocal.net>
2020-05-15 17:17:45 -0700

    got most of the stuff onto the page

---
+++ src/Form.js
@@ -0,0 +1,168 @@
+import React, { useState } from 'react';
+
+function Form (){
+
+    const [ formState , setFormState ] = useState({
+        name:'',
+        size:'',
+        sauce:'',
+        pepperoni:false, 
+        garlic:false, 
+        pineapple:false, 
+        olives:false, 
+        mushrooms:false, 
+        onions:false, 
+        request:'',
+    });
+
+
+    const inputChange = e => {
+        e.persist();
+        // console.log("input changed!", e.target.value, e.target.checked);
+        let value =
+          e.target.type === "checkbox" ? e.target.checked : e.target.value;
+        setFormState({ ...formState, [e.target.name]: value });
+      };
+
+    const formSubmit = e => {
+        e.preventDefault();
+        console.log("form submitted!");
+    };
+
+    return (
+       <form onSubmit={formSubmit}>
+           <div className='name'>
+           <label htmlFor="name">
+               Name 
+            <input
+          type="text"
+          name="name"
+          id="name"
+          value={formState.name}
+          onChange={inputChange}
+        />
+           </label>
+           <br/>
+
+           </div>
+           <div className='size'>
+           <label htmlFor="size">
+               Choose Your Size
+               <select
+               value={formState.size}
+               name="size"
+               id="size"
+               onChange={inputChange}>
+                   <option value="">----</option>
+                   <option value="X-Small">X-Small</option>
+                   <option value="Small">Small</option>
+                   <option value="Medium">Medium</option>
+                   <option value="Large">Large</option>
+                   <option value="X-Large">X-Large</option>
+               </select>
+           </label>
+           <br/>
+           </div>
+
+           <div className='sauce'>
+           <label htmlFor="sauce">
+               Choose Your Sauce
+               <select
+               value={formState.sauce}
+               name="sauce"
+               id="sauce"
+               onChange={inputChange}>
+                   <option value="">----</option>
+                   <option value="Red">Red Sauce</option>
+                   <option value="BBQ">BBQ</option>
+                   <option value="Garlic">Garlic White Sauce</option>
+                   <option value="Alfredo">Alfredo</option>
+               </select>
+           </label>
+           <br/>
+           </div>
+
+           <div className='toppings'>
+           <label htmlFor="toppings">
+           Pick Your Toppings</label>
+           <br/>
+        <input
+          type="checkbox"
+          id="pepperoni"
+          name="pepperoni"
+          checked={formState.pepperoni}
+          onChange={inputChange}
+        />
+        <label htmlFor="pepperoni">Pepperoni</label>
+        <br/>
+    
+        <input
+          type="checkbox"
+          id="garlic"
+          name="garlic"
+          checked={formState.garlic}
+          onChange={inputChange}
+        />
+        <label htmlFor="garlic">Garlic</label>
+        <br/>
+         
+        <input
+          type="checkbox"
+          id="pineapple"
+          name="pineapple"
+          checked={formState.pineapple}
+          onChange={inputChange}
+        />
+        <label htmlFor="pineappple">Pineapple</label>
+        <br/>
+         
+        <input
+          type="checkbox"
+          id="olives"
+          name="olives"
+          checked={formState.olives}
+          onChange={inputChange}
+        />
+        <label htmlFor="olives">Black Olives</label>
+        <br/>
+         
+        <input
+          type="checkbox"
+          id="mushrooms"
+          name="mushrooms"
+          checked={formState.mushrooms}
+          onChange={inputChange}
+        />
+        <label htmlFor="mushrooms">Mushrooms</label>
+        <br/>
+        
+        <input
+          type="checkbox"
+          id="onions"
+          name="onions"
+          checked={formState.onions}
+          onChange={inputChange}
+        />
+         <label htmlFor="onions">Onions</label>
+        <br/>
+           </div>
+
+           <div className="request">
+           <label htmlFor="request">Special Intructions</label>
+           <br/>
+            <textarea
+            name="request"
+            id="request"
+            placeholder="Put instructions here!"
+            value={formState.request}
+            onChange={inputChange}
+            />
+        
+
+           </div>
+
+       </form>
+    );
+};
+
+export default Form;
